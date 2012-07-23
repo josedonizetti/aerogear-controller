efe4022de1ac4577ecf56c4178722594456d9093
Bruno Oliveira <bruno@abstractj.org>
2012-07-23 18:12:56 -0300

    Moving deltaspike dependencies to aerogear security

--- src/main/java/org/jboss/aerogear/controller/spi/DeltaSpikeAdapter.java
+++
@@ -1,9 +0,0 @@
-package org.jboss.aerogear.controller.spi;
-
-@Deprecated
-public class DeltaSpikeAdapter implements AuthenticationProvider{
-    @Override
-    public boolean hasRole(AerogearUser user, String param) {
-        return false;
-    }
-}
--- src/main/java/org/jboss/aerogear/controller/spi/SecurityProvider.java
+++ src/main/java/org/jboss/aerogear/controller/spi/SecurityProvider.java
@@ -2,6 +2,6 @@ package org.jboss.aerogear.controller.spi;
 
 import org.apache.deltaspike.security.api.User;
 
-public interface AuthenticationProvider {
+public interface SecurityProvider {
     public boolean hasRole(User user, String param);
 }
